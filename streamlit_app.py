f72e0fad222455e79fc15f6a2b6994388c404b13
owlsociety <ws.jeong@owlsociety.kr>
2026-02-19 15:23:49 +0900

    Import date along with datetime in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import rcParams
-from datetime import datetime
+from datetime import datetime, date
 import matplotlib.font_manager as fm
 
 # 데이터 불러오기
